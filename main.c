19aee955973826b44677dd149ab2de310fbcd52f
francescacoo@gmail.com <francescacoo@gmail.com>
2017-03-07 15:05:20 +0000

    updated

--- main.c
+++ main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <time.h>
 
 typedef struct
 {
@@ -14,31 +15,50 @@ typedef struct
     int EmployeeNumber;//employee number
     int jobNumber;
     char* customer;
-
+    time_t dueDate;         //The job due date
+    time_t completedDate;  //The job's actual completion date
 }Job;
 
 
 int display_menu();
-Employee** load_data(char* filename, int *totLines);
+
+Employee** load_data_employee(char* filename, int *totLines);
+Job** load_data_job(char* filename, int *totLines2);
+
+void** load_data();
 
 int main() {
     int chosenOption = 0;
     Employee **arrayEmployees;    /* Array of employees*/
-    char **arrayJobs = NULL;    /* Array of jobs */
+    void** arrayData;
+    Job **arrayJobs;    /* Array of jobs */
     int totLines;         /* Total number of read lines */
+    int totLines2;         /* Total number of read lines */
 
     chosenOption = display_menu();
-    printf("you selected %d", chosenOption);
+    printf("you selected %d\n", chosenOption);
+    arrayData=load_data();
 
-    arrayEmployees=load_data("employeeFile.txt", &totLines);
+    arrayEmployees=arrayData[1];
 
-    //  arrayJobs=load_data("jobsFile.txt", &totLines);
     for (int i = 0; i < 3; i++) {
-        printf("[%d]: %d\n", (i + 1), arrayEmployees[i]->Number);
-        printf("[%d]: %s\n", (i + 1), arrayEmployees[i]->firstName);
-        printf("[%d]: %s\n", (i + 1), arrayEmployees[i]->lastName);
+        printf("\n[%d]: %d", (i + 1), arrayEmployees[i]->Number);
+        printf("  %s", arrayEmployees[i]->firstName);
+        printf(" %s", arrayEmployees[i]->lastName);
 
     }
+
+    arrayJobs=arrayData[0];
+
+        for (int i = 0; i < 2; i++) {
+        printf("\n[%d]: %d", (i + 1), arrayJobs[i]->EmployeeNumber);
+        printf("%d",  arrayJobs[i]->jobNumber);
+        printf("%s", arrayJobs[i]->customer);
+        printf(" %li", arrayJobs[i]->dueDate);
+        printf(" %li", arrayJobs[i]->completedDate);
+
+    }
+
         return 0;
     }
 
@@ -75,8 +95,21 @@ int main() {
     }
 
 // Function 2:  load_data
+void** load_data() {
 
-Employee** load_data(char *filename, int *totLines) {
+    void **mypointer = NULL;
+//    Job **arrayJobs;    /* Array of jobs */
+    int totLines=0;         /* Total number of read lines */
+    int totLines2=0;         /* Total number of read lines */
+    mypointer = calloc(2, sizeof(void*));
+    mypointer[1]=load_data_employee("employeeFile.txt", &totLines);
+    mypointer[0]=load_data_job("jobFile.txt", &totLines2);
+    return mypointer;
+
+}
+
+
+Employee** load_data_employee(char *filename, int *totLines) {
 
 
         printf("load data started");
@@ -85,7 +118,6 @@ Employee** load_data(char *filename, int *totLines) {
 //if the file doesn’t exist:
         while ((employeeFile = fopen(filename, "r")) == NULL) {
             fprintf(stderr, "Error opening the file");
-
         }
 
         Employee** array=NULL;
@@ -107,19 +139,13 @@ Employee** load_data(char *filename, int *totLines) {
 
         array = malloc(line_count * sizeof(Employee*));
 
-        /* Allocate an array of pointers to strings
-         * (one item per line). */
-        //array = malloc(line_count * sizeof(char *));
         if (array == NULL) {
             // return NULL; /* Error */
         }
 
-
-
         /* Read each line from file and deep-copy in the array. */
         for (i = 0; i < line_count; i++) {
             array[i] = malloc(sizeof(Employee));
-
 
             fscanf(employeeFile, "%s", temp); //Employee Last Name
             array[i]->Number = (int) strtol(temp, (char **)NULL, 10);
@@ -131,20 +157,86 @@ Employee** load_data(char *filename, int *totLines) {
             fscanf(employeeFile, "%s", temp); //Employee Last Name
             array[i]->lastName = calloc(strlen(temp) + 1, sizeof(char));
             strcpy(array[i]->lastName, temp);
-            /* Copy the line into the newly allocated space. */
-            //       strcpy(array[i], line);
         }
 
         /* Write output param */
-        *totLines = line_count;
+ //       *totLines = line_count;
 
         /* Return the array of lines */
         return array;
 
     }
 
+// testing function 2
 
 
+
+
+Job** load_data_job(char *filename, int *totLines2) {
+
+    printf("\n job load data started");
+    FILE *jobFile; // Input file
+
+//if the file doesn’t exist:
+    while ((jobFile = fopen(filename, "r")) == NULL) {
+        fprintf(stderr, "Error opening the file");
+    }
+
+    Job** array = NULL;
+    int i;                   /* Loop counter */
+    char line[100];           /* Buffer to read each line */
+    int line_count;          /* Total number of lines */
+    char temp[200];
+    /* Clear output parameter. */
+    *totLines2 = 0;
+
+    /* Get the count of lines in the file */
+    line_count = 0;
+    while (fgets(line, sizeof(line), jobFile) != NULL) {
+        line_count++;
+    }
+
+    /* Move to the beginning of file. */
+    rewind(jobFile);
+
+    array = malloc(line_count * sizeof(Job*));
+
+    if (array == NULL) {
+        // return NULL; /* Error */
+    }
+
+    /* Read each line from file and deep-copy in the array. */
+    for (i = 0; i < line_count; i++) {
+        array[i] = malloc(sizeof(Job));
+
+        fscanf(jobFile, "%s", temp); //Employee Last Name
+        array[i]->EmployeeNumber = (int) strtol(temp, (char **)NULL, 10);
+
+        fscanf(jobFile, "%s", temp); //Employee Last Name
+        array[i]->jobNumber = (int) strtol(temp, (char **)NULL, 10);
+
+        fscanf(jobFile, "%s", temp); //Employee Last Name
+        array[i]->customer = calloc(strlen(temp) + 1, sizeof(char));
+        strcpy(array[i]->customer, temp);
+
+        //Get the Due Date and store in the data structure
+        fscanf(jobFile, "%s", temp); //Due Date
+        array[i]->dueDate = (time_t)strtoull(temp, NULL, 10);
+
+        //Get the Completion Date and store in the data structure
+        fscanf(jobFile, "%s", temp); //Completion Date
+        array[i]->completedDate = (time_t)strtoull(temp, NULL, 10);
+    }
+
+    /* Write output param */
+    *totLines2 = line_count;
+
+    /* Return the array of lines */
+    return array;
+
+    /* Allocate an array of pointers to strings
+     * (one item per line). */
+}
 // Function 3:  add_new_employee
 
 // Function 4:  add_new_job
